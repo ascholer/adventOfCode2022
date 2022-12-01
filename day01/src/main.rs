6f1ecbdaf3ed4d5027f6c1c5d39ecd3a78004365
Andrew Scholer <ascholer@chemeketa.edu>
2022-11-30 23:39:04 -0800

    Cleanup

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,14 +2,13 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
     let s = fs::read_to_string("data.txt").unwrap();
-    let elves = s.split("\n\n").map(|l| l.split("\n"));
-    let elves = elves.map(|e| e.map(|s| str::parse::<i32>(s).unwrap()));
-    let mut elves: Vec<i32> = elves.map(|e| e.sum::<i32>()).collect();
-    elves.sort_by(|a, b| b.cmp(a));
+    let elves = s.split("\n\n").map(|l| l.split("\n").map(|s| str::parse::<i32>(s).unwrap()));
+    let mut elf_totals: Vec<i32> = elves.map(|e| e.sum::<i32>()).collect();
+    elf_totals.sort_by(|a, b| b.cmp(a));
     
-    println!("Part 1: {:?}", elves[0]);
-    
-    let total = elves[0..3].iter().sum::<i32>();
+    println!("Part 1: {:?}", elf_totals[0]);
+
+    let total = elf_totals[0..3].iter().sum::<i32>();
     println!("Part 2: {:?}", total);
 
     Ok(())
